afc7073ce6ab30106ca21a3a20b4ef439e86b1d0
brunobandelli <brunobandelli@gmail.com>
2021-11-15 15:00:01 -0300

    Ambiente finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-const nome = 'Luis'
-const sobrenome = "Otávio"
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+const nome = 'Luis';
+const sobrenome = 'Otávio';
+
+console.log(nome, sobrenome)
