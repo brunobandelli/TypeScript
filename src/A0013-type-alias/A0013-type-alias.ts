70038cb0490ffa902bfe88ef098f90f77eb19416
brunobandelli <brunobandelli@gmail.com>
2022-06-25 23:42:49 -0300

    Type-Alias

---
+++ src/A0013-type-alias/A0013-type-alias.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+
+type Idade = number;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: string;
+};
+
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+const pessoa: Pessoa = {
+  idade: 30,
+  nome: 'Luiz',
+  salario: 200_000, // 200000
+};
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+  return { ...pessoa, corPreferida: cor };
+}
+
+
+console.log(setCorPreferida(pessoa, 'Azul'));
+console.log(pessoa);
